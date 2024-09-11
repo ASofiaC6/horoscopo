a132dd36db8855add06415e80ac9f64ee62fba7d
ASofiaC6 <anasofiahh@isb.edu.mx>
2024-09-11 08:24:24 -0600

    j

---
+++ main.py
@@ -0,0 +1,33 @@
+print("Ingresar los datos en formato numérico")
+
+#Declaramos variables
+dia=int(input("Ingresa tu día de nacimiento: "))
+mes=int(input("Ingresa tu mas: "))
+
+#Se realiza la logica del programa
+if(mes==3 and dia>=21 or mes==4 and dia<=19):
+    signo="Tu eres Aries"
+elif(mes==4 and dia>=20 or mes==5 and dia<=20):
+    signo="Tu eres Tauro"
+elif(mes==5 and dia>=21 or mes==6 and dia<=20):
+    signo="Tu eres Géminis"
+elif(mes==6 and dia>=21 or mes==7 and dia<=22):
+    signo="Tu eres Cáncer"
+elif(mes==7 and dia>=23 or mes==8 and dia<=22):
+    signo="Tu eres Leo"
+elif(mes==8 and dia>=23 or mes==9 and dia<=22):
+    signo="Tu eres Virgo"
+elif(mes==9 and dia>=23 or mes==10 and dia<=22):
+    signo="Tu eres Libra"
+elif(mes==10 and dia>=23 or mes==11 and dia<=21):
+    signo="Tu eres Escorpio"
+elif(mes==11 and dia>=22 or mes==12 and dia<=21):
+    signo="Tu eres Sagitario"
+elif(mes==12 and dia>=22 or mes==1 and dia<=19):
+    signo="Tu eres Capricornio"
+elif(mes==1 and dia>=20 or mes==2 and dia<=18):
+    signo="Tu eres Acuario"
+elif(mes==2 and dia>=19 or mes==3 and dia<=20):
+    signo="Tu eres Piscis"
+
+print(signo)
